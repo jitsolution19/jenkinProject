cea48e668f6130df2ea45fb27757fffba253ac0e
jitsolution19 <jitsolution19@gmail.com>
2018-08-07 00:10:36 +0530

    update

--- JenkinsProject/src/HelloProject.java
+++ JenkinsProject/src/HelloProject.java
@@ -4,7 +4,7 @@ public class HelloProject {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello jitendra");
-		
+		System.out.println("Akansha");
 		int i=0;
 		
 		while(i<10)
