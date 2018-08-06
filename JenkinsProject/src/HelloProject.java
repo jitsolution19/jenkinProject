cccb91d47619d06a3eae2d44eb365b3b64bf087c
jitsolution19 <jitsolution19@gmail.com>
2018-08-07 00:01:43 +0530

    adding loop

--- JenkinsProject/src/HelloProject.java
+++ JenkinsProject/src/HelloProject.java
@@ -4,6 +4,14 @@ public class HelloProject {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello jitendra");
+		
+		int i=0;
+		
+		while(i<10)
+		{
+			System.out.println("hello");
+			i++;
+		}
 	}
 
 }
