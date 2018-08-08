31f6a649b52779395d5adeb2d92eb6486765e045
jitsolution19 <jitsolution19@gmail.com>
2018-08-08 08:51:25 +0530

    updated stock information

--- JenkinsProject/src/HelloProject.java
+++ JenkinsProject/src/HelloProject.java
@@ -5,13 +5,27 @@ public class HelloProject {
 		System.out.println("Hello");
 		System.out.println("Hello jitendra");
 		System.out.println("Akansha");
-		int i=0;
-		
-		while(i<10)
-		{
-			System.out.println("hello");
-			i++;
-		}
+	System.out.println("EPS= Total Earning/Number of Outstanding Share\n"+
+			"this help in comparing the stocks of same companies\n"+
+
+			"PE Ratio= Price of Stock/Earning per share\n"+
+			"PERatio upward\n"+
+
+			"Lower the PE the better the valuation of company\n"+
+
+			"Price to Book Value =Current market Price/Book value of Share\n"+ 
+			"P/B Ratio <1\n"+ 
+
+			"Debt\n"+ 
+			"DebtToEquity Ratio\n"+
+
+			"D/E Ratio = Total Laiblities/Total Asset\n"+
+			"D/E<1 good valution\n"+ 
+
+			"Return on Equity\n"+
+
+			"ROE=Net Profit/Total Equity\n"+
+			"ROE upward are efficient ");	
 	}
 
 }
